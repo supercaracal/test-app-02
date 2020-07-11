509525ae0cf5b32fcd8fc4fba6bbe811393bc516
Taishi Kasuga <kasuga@livesense.co.jp>
2020-07-11 19:38:26 +0900

    Fix ssh opts

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, "test-app-02"
 set :repo_url, "https://github.com/supercaracal/test-app-02.git"
 set :deploy_to, "/opt/app/test-app-02"
-set :ssh_options, verify_host_key: :secure
+set :ssh_options, user: ENV['DEPLOY_USER'], auth_methods: %w[publickey]
 set :bundle_flags, '--quiet'
 set :bundle_path, -> { nil }
 set :bundle_without, nil
